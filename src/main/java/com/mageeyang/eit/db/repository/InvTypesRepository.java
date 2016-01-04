d983221341c8aadda10f7357c51d8df96acd0e6f
Magee_Yang <ym31316120@gmail.com>
2016-01-04 17:15:20 +0800

    12

--- src/main/java/com/mageeyang/eit/db/repository/InvTypesRepository.java
+++ src/main/java/com/mageeyang/eit/db/repository/InvTypesRepository.java
@@ -3,6 +3,7 @@ package com.mageeyang.eit.db.repository;
 import com.mageeyang.eit.db.model.InvtypesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -11,4 +12,5 @@ import java.util.List;
 public interface InvTypesRepository extends JpaRepository<InvtypesEntity,Integer> {
     public List<InvtypesEntity> findBytypeId(int typeid);
     public List<InvtypesEntity> findBymarketGroupId(Long marketGroupId);
+    public List<InvtypesEntity> findBymarketGroupIdIn(Collection<Long> arg);
 }
